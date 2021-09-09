3de527d243c3c16a7ddd3309ceab5ba4b13506f5
linxiao <linx@taxsoft.cn>
2021-09-09 10:10:08 +0800

    buildSrc

---
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -0,0 +1,19 @@
+/**
+ * @author haizhuo
+ * @description 编译配置信息
+ */
+object BuildConfig {
+    const val compileSdkVersion = 30
+    const val buildToolsVersion ="30.0.3"
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 30
+
+    const val applicationId ="com.lx.componentmvvm"
+    const val testInstrumentationRunner="androidx.test.runner.AndroidJUnitRunner"
+
+    var versionCode = 1
+    var versionName = "1.0"
+
+    var gradle_version = "4.2.2"
+    var kotlin_version = "1.5.21"
+}
